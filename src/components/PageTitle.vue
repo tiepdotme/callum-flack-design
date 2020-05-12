bfa847445921a5b08deeedfc611ef84b6cb2b754
Tiep <50001808+tiepdotme@users.noreply.github.com>
2020-05-12 19:09:57 +0700

    Update PageTitle.vue

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -36,7 +36,7 @@
             </saber-link>
             <br /><br />
           </template> 
-          <template v-if="$route.path === '/'">Có phải bạn tìm mua mgid không? xem <template v-if="$route.path === '/'">
+          <template v-if="$route.path === '/'">Có phải bạn tìm mua mgid không? xem 
             <saber-link to="/mgid" class="Link text-brand-red">
               báo giá account mgid &#8594;
             </saber-link>
