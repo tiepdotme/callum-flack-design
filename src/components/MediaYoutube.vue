bf0ad14d93fb8f0fdadd99f5a67f40bd786210e3
Tiep <50001808+tiepdotme@users.noreply.github.com>
2020-05-23 12:42:06 +0700

    Update MediaYoutube.vue

--- src/components/MediaYoutube.vue
+++ src/components/MediaYoutube.vue
@@ -101,7 +101,7 @@ export default {
   },
   computed: {
     youtubeSrc() {
-      return `https://www.youtube.com/embed/${this.src}?autoplay=1&mute=1&origin=https://phusa.me&showinfo=0&iv_load_policy=3&controls=0`;
+      return `https://www.youtube.com/embed/${this.src}?autoplay=1&mute=0&origin=https://phusa.me&showinfo=0&iv_load_policy=3&controls=0`;
     },
     aspectClasses() {
       return [
